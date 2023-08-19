43f6e692a8c84425df10db8569ada9440becef8d
zeSurvivalDeviant <142660385+zeSurvivalDeviant@users.noreply.github.com>
2023-08-19 18:21:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,40 @@
 <?php <head><title>Ze Survival Stories</title>
-  echo "<meta charset="utf-8">
-  <link rel="shortcut icon" href="image/profile.png" type="image/x-icon">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  echo "<meta charset='utf-8'>
+  <link rel='shortcut icon' href='image/profile.png' type='image/x-icon'>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css'>
+  <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js'></script>
+  <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js'></script>
 </head>
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
+<nav class='navbar navbar-inverse'>
+  <div class='container-fluid'>
+    <div class='navbar-header'>
+      <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#myNavbar'>
+        <span class='icon-bar'></span>
+        <span class='icon-bar'></span>
+        <span class='icon-bar'></span>                        
       </button>
-      <a class="navbar-brand" href="#">Ze Survival Stories</a>
+      <a class='navbar-brand' href='#'>Ze Survival Stories</a>
     </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">My DeviantArt content<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">2020-2021</a></li>
-            <li><a href="#">2021-2022</a></li>
-            <li><a href="#">2022-2023</a></li>
-            <li><a href="#">2023-2024</a></li>
+    <div class='collapse navbar-collapse' id='myNavbar'>
+      <ul class='nav navbar-nav'>
+        <li class='active'><a href='#'>Home</a></li>
+        <li class='dropdown'>
+          <a class='dropdown-toggle' data-toggle='dropdown' href='#'>My DeviantArt content<span class='caret'></span></a>
+          <ul class='dropdown-menu'>
+            <li><a href='#'>2020-2021</a></li>
+            <li><a href='#'>2021-2022</a></li>
+            <li><a href='#'>2022-2023</a></li>
+            <li><a href='#'>2023-2024</a></li>
           </ul>
         </li>
-        <li><a href="#">My Patreon content</a></li>
-        <li><a href="#">Valentin's Story on DeviantArt</a></li>
+        <li><a href='#'>My Patreon content</a></li>
+        <li><a href='#'>Valentin's Story on DeviantArt</a></li>
       </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="connexion/connexion.html"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      <ul class='nav navbar-nav navbar-right'>
+        <li><a href='#'><span class='glyphicon glyphicon-user'></span> Sign Up</a></li>
+        <li><a href='connexion/connexion.html'><span class='glyphicon glyphicon-log-in'></span> Login</a></li>
       </ul>
     </div>
   </div>
